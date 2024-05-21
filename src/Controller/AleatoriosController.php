dbcbdd84e07e9100d5887550a74773d472cc41ae
admin <admin@H310M-S2H-2-0-f26854f8.localdomain>
2024-05-21 20:48:18 +0200

    2024-05-21 [tarde]

--- src/Controller/AleatoriosController.php
+++ src/Controller/AleatoriosController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/aleatorios', name: 'app_aleatorios')]
+#[Route('/aleatorios', name: 'app_aleatorios_')]
 class AleatoriosController extends AbstractController
 {
-    #[Route('/ej1', name: 'app_aleatorios')]
+    #[Route('/ej1', name: 'app_aleatorios1')]
     public function index(): Response
     {
         return $this->render('aleatorios/index.html.twig', [
@@ -17,7 +17,7 @@ class AleatoriosController extends AbstractController
         ]);
     }
 
-    #[Route('/ej2', name: 'app_aleatorios_aleatorios2')]
+    #[Route('/ej2', name: 'app_aleatorios2')]
     public function index2(): Response
     {
         return $this->render('aleatorios/index.html.twig', [
@@ -28,7 +28,27 @@ class AleatoriosController extends AbstractController
     public function index3(): Response
     {
         return $this->render('aleatorios/index.html.twig', [
-            'controller_name' => 'Pagina2 Aleatorios',
+            'controller_name' => 'Pagina2 YAML',
+        ]);
+    }
+
+    #[Route('/ej4/{num1}/{num2}', name: 'app_aleatorios4')]
+    public function index4(int $num1, int $num2): Response
+    {
+        $aleatorio = rand($num1, $num2);
+
+        return $this->render('aleatorios/index.html.twig', [
+            'controller_name' => $aleatorio,
+        ]);
+    }
+
+    
+    public function index5(int $num1, int $num2): Response
+    {
+        $aleatorio = rand($num1, $num2);
+        
+        return $this->render('aleatorios/index.html.twig', [
+            'controller_name' => $aleatorio,
         ]);
     }
 }
